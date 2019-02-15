22750aef3bbc9b71ffa8018c0cdf68e0fe8079c0
TsaiDC <neko.artin@gmail.com>
2019-02-15 18:15:43 +0800

    [#3,lengthOfLongestSubstring]
    Test String
    
    command: git push -u origin master-dev

--- lengthOfLongestSubstring.cpp
+++ lengthOfLongestSubstring.cpp
@@ -1,13 +1,18 @@
-//g++ main.cpp addTwoNumbers.cpp -o tool
+//g++ *.cpp -o tool
 
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string>
+#include <memory.h>
+#include <map>
 
 #include "apiheader.h"
 
 using namespace std;
+
+typedef map<string, int> StrIntMap;
+typedef pair<string, int> StrIntPair;
 
 /*
 Given a string, find the length of the longest substring without repeating characters.
@@ -30,12 +35,36 @@ Explanation: The answer is "wke", with the length of 3.
              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
 
-int lengthOfLongestSubstring(string s) {
+int lengthOfLongestSubstring(string s) 
+{
+    printf("[DBG] %s(%d) s: %s\n", __FUNCTION__, __LINE__, s.c_str());
+    
+    int CIDX = 0;
+    int Map[200];    
+    int *idx = new int[s.length()];
+    memset(Map, 0, sizeof(Map[0])*200);
+    memset(idx, 0, sizeof(int)*s.length());
+
+    for(int i=0; i<s.length(); ++i)
+    {
+        printf ("%c -> %d \n", s.at(i), (int)s.at(i));
+        if(i==0) {
+        }
+        else if (Map[i] ==0) {
+            CIDX++;
+        }
+        else {
+        }
+        
+        idx[i] = CIDX
+    }
+    
+    delete []idx; 
     return 0;
 }
 
 void Test_lengthOfLongestSubstring()
 {
     printf("[DBG] %s(%d) %s\n", __FUNCTION__, __LINE__, __TIME__);
-    lengthOfLongestSubstring(NULL);
+    lengthOfLongestSubstring("abcabcbb");
 }
