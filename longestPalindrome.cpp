ffdef41a2664b2e34a2c3c3c5abcb4c5f5643f60
TsaiDC <neko.artin@gmail.com>
2019-02-22 18:28:57 +0800

    [#5.longestPalindrome]
    modifying..
    
    git push -u origin master-dev

--- longestPalindrome.cpp
+++ longestPalindrome.cpp
@@ -26,11 +26,33 @@ Output: "bb"
 
 string longestPalindrome(string s)
 {
-    return " ";
+    int j1=0, j2 = 0, k=0, m=1;
+    for(int i=0; i<s.length(); ++i)
+    {
+        cout<<"+++"<<s.at(i)<<endl;
+        j1=i-m;
+        j2=i+m;
+        while ( j1>=0 && j2<s.length())
+        {
+            if(s.at(i) != s.at(i+1)){
+                //aba, b!=a
+                if(s.at(j) == s.at(j2)) {
+                    ++m;
+                }
+            }
+            else {
+                //abba, b==b
+            }
+        }
+        cout<<"---"<<s.at(i)<<endl;
+    }
+
+    return "TBD";
 }
 
 void Test_longestPalindrome()
 {
     printf("[DBG] %s(%d) %s\n", __FUNCTION__, __LINE__, __TIME__);
-    
+    string input = "cabad";
+    cout<<"Input: "<<input<<" Result: "<<longestPalindrome(input);    
 }
