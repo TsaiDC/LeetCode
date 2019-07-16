919e0f8053f57ef90e1d1e051b1f386e36f156f7
TsaiDC <neko.artin@gmail.com>
2019-07-16 18:13:47 +0800

    [#00.Sorting Test]
    QuickSort, Merge Sort
    
    git push origin HEAD:master-dev

--- 00_Sortings.cpp
+++ 00_Sortings.cpp
@@ -12,13 +12,16 @@ using namespace std;
 
 #define NONE "\033[m"
 #define RED  "\033[0;32;31m"
+#define GREEN "\033[0;32;32m"
 #define CYAN "\033[0;36m"
 
 #define DEBUG 1
 #if DEBUG
+#define LOGI(...) {printf(GREEN "[I] %s(%d): " NONE, __FUNCTION__, __LINE__);printf( __VA_ARGS__);}
 #define LOGD(...) {printf(CYAN "[D] %s(%d): " NONE, __FUNCTION__, __LINE__);printf( __VA_ARGS__);}
 #define LOGE(...) {printf(RED "[E] %s(%d): " NONE, __FUNCTION__, __LINE__);printf( __VA_ARGS__);}
 #else
+#define LOGI(...)
 #define LOGD(...)
 #define LOGE(...)
 #endif
@@ -28,6 +31,16 @@ using namespace std;
 Sortings:
 http://notepad.yehyeh.net/Content/Algorithm/Sort/Sort.php
 */
+
+void showList(int* pData, int leng)
+{
+    printf("<-- ");
+    for(int i=0; i<leng; ++i)
+    {
+        printf("%d ", pData[i]);
+    }
+    printf("-->\n");
+}
 
 void doInsertionSort(int* pData, int leng)
 {
@@ -101,30 +114,113 @@ void doBubbleSort(int* pData, int leng)
 	}
 }
 
-void doQuickSort(int* pData, int i, int j, int p)
+void doQuickSort(int* pData, int i, int j)
 {
-    LOGD("\n");
+//    LOGD("Range: %d -> %d\n", i, j);
+#if 0
+    LOGD("+++Input\n");
+    printf("->");
+    for(int xx=i; xx<=j; ++xx)
+    {
+        printf("%d ", pData[xx]);
+    }
+    printf("\n");
+    LOGD("---Input\n");
+#endif
+
+    int p = j;
+    int x = i, y = j-1;
+    int tmp = 0;
+    
+    if(x>y) {
+//        LOGE("Stop\n");
+        return;
+    }
+    
+    while(x<y)
+    {
+        while (x<=y && (pData[x] < pData[p])){++x;}
+        while (y>=x && (pData[y] > pData[p])){--y;}    
+        
+//        LOGD("P(%d):%d, x(%d):%d, y(%d):%d\n", p, pData[p], x, pData[x], y, pData[y]);
+        
+        if(x<y) {
+            tmp = pData[x];
+            pData[x] = pData[y];
+            pData[y] = tmp;
+        }
+#if 0   
+        printf("--");
+        for(int xx = i; xx<j; ++xx)
+        {
+            printf("%d ",pData[xx]);
+        }
+        printf("\n");
+#endif
+    }
+
+//    LOGE("Final Swap P(%d):%d, x(%d):%d, y(%d):%d\n", p, pData[p], x, pData[x], y, pData[y]);
+    if (pData[x] > pData[p]) {
+        tmp = pData[x];
+        pData[x] = pData[p];
+        pData[p] = tmp;
+    }
+#if 0
+    printf("<-");
+    for(int xx = i; xx<=j; ++xx)
+    {
+        printf("%d ",pData[xx]);
+    }
+    printf("\n");
+#endif
+//    LOGD("p: %d,  0~%d <=> %d~%d \n", x, x-1, x+1, j);
+    doQuickSort(pData, 0, x-1);
+    doQuickSort(pData, x+1, j);
+}
+
+void doMergeSort(int* pData, int i, int j)
+{
+    LOGD("Range: %d -> %d, (%d <-> %d), (%d <->%d)\n", i, j, i, i+(j-i)/2, i+(j-i)/2 + 1, j);
+
+    int tmp = 0;
+    if(j-i == 0 || j-i == 1) {
+        LOGD("Single Item i: %d, j: %d\n", i, j);
+        if(pData[i] > pData[j]) {
+            tmp = pData[i];
+            pData[i] = pData[j];
+            pData[j] = tmp;            
+        }
+        showList(pData, 10);
+        return;
+    }
+
+    doMergeSort(pData, i, i+(j-i)/2);
+    doMergeSort(pData, i+(j-i)/2 + 1, j);
+    LOGD("Start Merge i: %d, j: %d\n", i, j);
 }
 
 void Test_sorting()
 {
-    LOGD("%s\n", __TIME__);
+    LOGI("%s\n", __TIME__);
 
-	int iData [] = {72,63,42,27,79,3,32,71,88,73};
-//    int iData [] = {75,98,97,12,82,78,91,21,72,70};
+//	int iData [] = {72,63,42,27,79,3,32,71,88,73};
+    int iData [] = {75,98,97,12,82,78,91,21,72,70};
 	int Num = sizeof(iData)/sizeof(iData[0]);	
-	LOGD("Input Data Num: %d\n", Num);
-	
+	LOGI("Input Data Num: %d\n", Num);
+	showList(iData, Num);
+
 //    doInsertionSort(iData, Num);
 //    doSelectionSort(iData, Num);
-    doBubbleSort(iData, Num);
-	    
-    LOGD("+++Result\n");
+//    doBubbleSort(iData, Num);
+//    doQuickSort(iData, 0, Num-1);
+    doMergeSort(iData, 0, Num-1);
+    
+    LOGI("+++Result\n");
     for(int i=0; i<Num; ++i)
     {
         printf("%d ", iData[i]);
     }
     printf("\n");
-    LOGD("---Result\n");
-    LOGD("Done \n");
+    LOGI("---Result\n");
+    LOGI("Done \n");
 }
