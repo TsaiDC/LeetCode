34d6db8c32ea2e1369cff5fb3e0613a9f863447b
TsaiDC <neko.artin@gmail.com>
2019-08-19 16:18:09 +0800

    [#19. Remove Nth Node From End of List]
    Resolved.
    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Remove Nth Node From End of List.
    Memory Usage: 8.5 MB, less than 97.37% of C++ online submissions for Remove Nth Node From End of List.
    
    git push origin HEAD:master-dev

--- 19_RemoveNthNode.cpp
+++ 19_RemoveNthNode.cpp
@@ -52,11 +52,105 @@ struct ListNode {
 };
 
 ListNode* removeNthFromEnd(ListNode* head, int n) {
-    return NULL;    
+
+    if(head == NULL || n <= 0) {
+        return head;
+    }
+    
+    ListNode *pHead = head;
+    vector<ListNode*> listp;
+    int idx = 0;
+    while (pHead != NULL)
+    {
+        listp.push_back(pHead);
+        pHead = pHead->next;
+    }
+/*
+    for(int i=0; i<listp.size(); ++i)
+    {
+        LOGD("%d \n", listp[i]->val);
+    }
+*/
+    idx = listp.size() - n;
+    if(idx<0) {
+        LOGE("Wrong Idx: %d\n", idx);
+        return head;
+    }    
+    LOGD("Remove: %d\n", listp[idx]->val);
+    int left = idx - 1;
+    int right= idx + 1;    
+
+
+    pHead = head;
+
+    //At Left
+    if(left < 0) {
+        if(listp.size() == 1) {
+            free(listp[0]);
+            return NULL;
+        }
+        else {
+            pHead = listp[1];
+            return pHead;
+        }
+    }
+
+    //At Right    
+    if(right >=listp.size()){
+        listp[left]->next = NULL;
+        free(listp[idx]);
+        return pHead;
+    }
+
+    //At Middle
+    listp[left]->next = listp[right];
+    free(listp[idx]);
+    return pHead;
 }
 
 void Test_removeNthFromEnd()
 {
     LOGD("%s\n", __TIME__);
+    ListNode *pHead = NULL, *pTail = NULL, *pTmp = NULL;
+    
+    for(int i=1; i<6; ++i)
+    {
+        pTmp = (ListNode*) malloc(sizeof(ListNode));
+        pTmp->val = i;
+        pTmp->next = NULL;
+        
+        if(pHead == NULL) {
+            pHead = pTmp;
+        }
+        if(pTail == NULL) {
+            pTail = pTmp;
+        }
+        else {
+            pTail->next = pTmp;
+            pTail = pTmp;
+        }
+    }
+
+    printf("Iput: ");
+    pTail = pHead;
+    while(pTail != NULL)
+    {
+        printf("%d ", pTail->val);
+        pTail = pTail->next;
+    }
+    printf("\n");
+
+    pHead = removeNthFromEnd(pHead, 1);
+    
+    printf("Oput: ");
+    while(pHead != NULL)
+    {
+        pTmp = pHead;
+        pHead = pHead->next;
+        printf("%d ", pTmp->val);
+        free(pTmp);
+    }
+    printf("\n");
+    
     LOGD("Done \n");
 }
