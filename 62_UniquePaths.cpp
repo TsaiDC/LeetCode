3e104d138355be4c97b1002930b729f49ddcd250
TsaiDC <neko.artin@gmail.com>
2019-08-30 17:12:40 +0800

    [#62. Unique Paths]
    Resolved.
    Runtime: 4 ms, faster than 55.11% of C++ online submissions for Unique Paths.
    Memory Usage: 8.4 MB, less than 60.94% of C++ online submissions for Unique Paths.
    
    git push origin HEAD:master-dev

--- 62_UniquePaths.cpp
+++ 62_UniquePaths.cpp
@@ -41,11 +41,67 @@ How many possible unique paths are there?
 */
 
 int uniquePaths(int m, int n) {
-    return 0;    
+    if(m == 1|| n == 1) {
+        return 1;
+    }
+    if(m>100 || n>100) {
+        return -1;
+    }
+    vector<int>VS;
+    
+    int T = (m+n-2) > 0 ? (m+n-2) : 1;
+    int B, S;
+    int up, down, result;
+
+    if(m>n){
+        B = m-1 > 0 ? m-1 : 1;
+        S = n-1 > 0 ? n-1 : 1;
+        
+    } else {
+        B = n-1 > 0 ? n-1 : 1;
+        S = m-1 > 0 ? m-1 : 1;
+    }
+    LOGD("%d!/(%d!*%d!)\n", T, B, S);
+    int i;
+    for(i = S; i > 0 ; --i)
+    {
+        VS.push_back(i);
+        printf("%d ", i);
+    }
+    printf("\n");
+    
+    for(i=0; i<VS.size(); ++i)
+    {
+        printf("%d ", VS[i]);
+    }
+    printf("\n");
+    
+    up = T;    
+    for(i = T-1; i > B; --i)
+    {
+        LOGD("Count, UP: %d, i: %d, New: %d\n", up, i, i*up);
+        up = i * up;
+        for(int j =0; j<VS.size(); ++j)
+        {
+            if(VS[j] == 0) {
+                continue;
+            }
+            if(up%VS[j] == 0) {
+                LOGD("Update: Up: %d, VS[%d]: %d\n", up, j, VS[j]);
+                up /= VS[j];
+                VS[j] = 0;
+            }
+        }
+    }
+    LOGD("Result: %d\n", up);
+    return up;
 }
 
 void Test_uniquePaths()
 {
     LOGD("%s\n", __TIME__); 
+//    int result = uniquePaths(10, 1764322560);
+    int result = uniquePaths(4, 6);
+    LOGD("Result: %d\n", result);
     LOGD("Done \n");
 }
