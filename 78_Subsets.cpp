9b054aeac24c981ad1cb5e3b74e8baf0b8a6a4c6
TsaiDC <neko.artin@gmail.com>
2020-04-01 15:22:00 +0800

    [#78. Subsets]
    Resolved
    Runtime: 4 ms, faster than 97.25% of C++ online submissions for Subsets.
    Memory Usage: 6.7 MB, less than 100.00% of C++ online submissions for Subsets.
    
    git push origin HEAD:master-dev

--- 78_Subsets.cpp
+++ 78_Subsets.cpp
@@ -53,71 +53,101 @@ Output:
 ]
 */
 
+//void combin(int n, int m, vector< vector<int> > &result);
+
 class Solution {
 public:
     vector< vector<int> > subsets(vector<int>& nums) {
         vector< vector<int> > result;
+        vector<int>index;
+        int length = nums.size();
+        for(int i=1; i<=length; ++i)
+            combin(length,i, result);
+        
+        for(int i=0; i<result.size(); ++i)
+        {
+            for(int j=0; j<result[i].size(); ++j)
+            {
+                result[i][j] = nums[result[i][j]-1];
+            }
+        }
+        result.push_back(index);
         return result;
     }
-};
 
-/*
-C(4,2) = 4 + C(3,1)
-C(3,2) = 3 + C(2,1)
-C(2,2) = 2 + C(1,1)
-
+    /*
+    C(4,2) = 4 + C(3,1)
+    C(3,2) = 3 + C(2,1)
+    C(2,2) = 2 + C(1,1)
 
 
-C(5,3) = 5 + C(4,2) = 5 + 4 + C(3,1)
-       = 5 + C(3,2) = 5 + 3 + C(2,1)
-       = 5 + C(2,2) = 5 + 2 + C(1,1)
-C(4,3) = 4 + C(3,2) = 4 + 3 + C(2,1)
-       = 4 + C(2,2) = 4 + 2 + C(1,1)
-C(3,3) = 3 + C(2,2) = 3 + 2 + C(1,1)
-*/
 
-void combin(int n, int m)
-{
-    static vector<int>index;
-//    if(n<m) return;
-    if(n<=0 || m<=0) return;
-    printf("C(%d, %d)\n", n, m);
-    if(m==1) {
-        LOGD("+++Done\n");
-        for(int j=n;j>0; j--)
-        {
-            index.push_back(j);
-            for(int i=0; i<index.size(); ++i)
-            {
-                printf("%d ", index[i]);
-            }
-            index.pop_back();
-            printf("\n");
-        }
-        LOGD("---Done\n");
-        return;
-    }
-    for(int i=n; i>=m; i--)
+    C(5,3) = 5 + C(4,2) = 5 + 4 + C(3,1)
+           = 5 + C(3,2) = 5 + 3 + C(2,1)
+           = 5 + C(2,2) = 5 + 2 + C(1,1)
+    C(4,3) = 4 + C(3,2) = 4 + 3 + C(2,1)
+           = 4 + C(2,2) = 4 + 2 + C(1,1)
+    C(3,3) = 3 + C(2,2) = 3 + 2 + C(1,1)
+    */
+    void combin(int n, int m, vector< vector<int> > &result)
     {
-        index.push_back(i);
-        printf(" = ");
-        for(int j=0; j<index.size(); ++j)
+        static vector<int>index;
+        if(n<=0 || m<=0) return;
+    //    printf("C(%d, %d)\n", n, m);
+        if(m==1) {
+    //        LOGD("+++Done\n");
+            for(int j=n;j>0; j--)
+            {
+                index.push_back(j);
+    #if 0
+                for(int i=0; i<index.size(); ++i)
+                {
+                    printf("%d ", index[i]);
+                }
+    #endif
+                result.push_back(index);
+                index.pop_back();
+    //            printf("\n");
+            }
+    //        LOGD("---Done\n");
+            return;
+        }
+        for(int i=n; i>=m; i--)
         {
-            printf("%d + ", index[j]);
-        }        
-        combin(i-1, m-1);
-        index.pop_back();
+            index.push_back(i);
+    #if 0
+            printf(" = ");
+            for(int j=0; j<index.size(); ++j)
+            {
+                printf("%d + ", index[j]);
+            }
+    #endif
+            combin(i-1, m-1, result);
+            index.pop_back();
+        }
+        
+    //    printf("\n");
     }
-    
-    printf("\n");
-}
+
+};
 
 void Test_Subsets()
 {
     LOGD("%s\n", __TIME__);
+    int arr1[] = {1,2,3};
+    int n = sizeof(arr1)/sizeof(arr1[0]);
+    vector<int> input1(arr1, arr1+n);
+
     Solution *solution = new Solution();
-    delete solution;
+    vector< vector<int> > result = solution->subsets(input1);
     
-    vector<int>index;
-    combin(5,3);
+    for(int i=0; i<result.size(); ++i)
+    {
+        LOGE("\n");
+        for(int j=0; j<result[i].size(); ++j)
+        {
+            LOGD("%d\n", result[i][j]);
+        }
+    }
+    delete solution;
 }
