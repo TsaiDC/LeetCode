2a2aefc4c3d5e3c7d70dc301479dfd687545fd7e
TsaiDC <neko.artin@gmail.com>
2020-05-20 00:52:12 +0800

    [#402. Remove K Digits]
    Init, 003
    
    git push origin HEAD:master-dev

--- 86_PartitionList.cpp
+++ 86_PartitionList.cpp
@@ -46,8 +46,8 @@ Output: 1->2->2->4->3->5
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode() : val(0), next(NULL) {}
+    ListNode(int x) : val(x), next(NULL) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
