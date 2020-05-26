01f4f9fd884913321b574952e859b0618eea269b
TsaiDC <neko.artin@gmail.com>
2020-05-26 14:13:30 +0800

    [#38. Count and Say]
    Resolved.
    Runtime: 4 ms, faster than 82.39% of C++ online submissions for Count and Say.
    Memory Usage: 6.7 MB, less than 100.00% of C++ online submissions for Count and Say.
    
    git push origin HEAD:master-dev

--- 38_CountandSay.cpp
+++ 38_CountandSay.cpp
@@ -60,12 +60,114 @@ Example 2:
 Input: 4
 Output: "1211"
 Explanation: For n = 3 the term was "21" in which we have two groups "2" and "1", "2" can be read as "12" which means frequency = 1 and value = 2, the same way "1" is read as "11", so the answer is the concatenation of "12" and "11" which is "1211".
+
+ 1.     1    one 1
+ 2.     11
+ 3.     21
+ 4.     1211
+ 5.     111221 
+ 6.     312211
+ 7.     13112221
+ 8.     1113213211
+ 9.     31131211131221
+10.     13211311123113112211
 */
 
 class Solution {
 public:
     string countAndSay(int n) {
+                
+        vector<int> Lstr;
+        vector<int> Rstr;
+        vector<int> count_vec;
+        Lstr.push_back(1);
+        
+        int count = 0;
+        int num = 0;
+
+        for(int i=1; i<n; i++)
+        {
+#if 0
+            LOGD("%d L Array: ", i+1);
+            for(int x=0;x<Lstr.size(); ++x)
+                printf("%d ", Lstr[x]);
+            printf(" -- ");
+#endif
+            //Convert to readable Array
+            count_vec.clear();
+            for(int j=0;j<Lstr.size(); ++j)
+            {
+                if(count_vec.empty()) {
+                    count_vec.push_back(Lstr[j]);
+                    continue;
+                }
+                else {
+                    if(Lstr[j]==count_vec.back()) {
+                        count_vec.push_back(Lstr[j]);
+                    }
+                    else {
+                        Rstr.push_back(count_vec.size());
+                        Rstr.push_back(count_vec.back());
+                        count_vec.clear();
+                        count_vec.push_back(Lstr[j]);
+                    }
+                }
+            }
+            Rstr.push_back(count_vec.size());
+            Rstr.push_back(count_vec.back());
+#if 0
+            for(int x=0;x<Rstr.size(); ++x)
+                printf("%d ", Rstr[x]);
+            printf("\n");
+#endif
+            //Convert to real array
+            Lstr.clear();
+            Lstr.assign(Rstr.begin(), Rstr.end());
+            Rstr.clear();
+        }
+        
+        return getString(Lstr);
+    }
+    
+    string getString(vector<int> &Lstr)
+    {
         string str="";
+        for(int i=0; i<Lstr.size(); i++)
+        {
+            switch(Lstr[i])
+            {
+            case 0:
+                str.push_back('0');
+                break;
+            case 1:
+                str.push_back('1');
+                break;
+            case 2:
+                str.push_back('2');
+                break;
+            case 3:
+                str.push_back('3');
+                break;
+            case 4:
+                str.push_back('4');
+                break;
+            case 5:
+                str.push_back('5');
+                break;
+            case 6:
+                str.push_back('6');
+                break;
+            case 7:
+                str.push_back('7');
+                break;
+            case 8:
+                str.push_back('8');
+                break;
+            case 9:
+                str.push_back('9');
+                break;
+            }            
+        }
         return str;
     }
 };
@@ -73,6 +175,10 @@ public:
 void Test_countAndSay()
 {
     LOGD("%s\n", __TIME__);
+    string str;
+    int n = 5;
     Solution *solution = new Solution();
+    str = solution->countAndSay(n);
+    LOGD("N: %d, Ans: %s\n", n, str.c_str());
     delete solution;
 }
