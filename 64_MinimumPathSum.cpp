f4067ffcaa45d74f1a48061dfcf682bde975fa12
TsaiDC <neko.artin@gmail.com>
2020-05-27 19:10:58 +0800

    [#64. Minimum Path Sum]
    Init. 002
    
    git push origin HEAD:master-dev

--- 64_MinimumPathSum.cpp
+++ 64_MinimumPathSum.cpp
@@ -33,7 +33,9 @@ using namespace std;
 https://leetcode.com/problems/minimum-path-sum/
 64. Minimum Path Sum
 
-Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+Given a m x n grid filled with non-negative numbers, 
+find a path from top left to bottom right which minimizes 
+the sum of all numbers along its path.
 
 Note: You can only move either down or right at any point in time.
 
@@ -45,6 +47,7 @@ Input:
   [1,5,1],
   [4,2,1]
 ]
+
 Output: 7
 Explanation: Because the path 1→3→1→1→1 minimizes the sum.
 */
@@ -59,7 +62,24 @@ public:
 void Test_minPathSum()
 {
     LOGD("%s\n", __TIME__);
+    int arr1[] = {1, 3, 1};
+    int arr2[] = {1, 5, 1};
+    int arr3[] = {4, 2, 1};
+    
+    int n1 = sizeof(arr1)/sizeof(arr1[0]);
+    int n2 = sizeof(arr2)/sizeof(arr2[0]);
+    int n3 = sizeof(arr3)/sizeof(arr3[0]);
+    vector<int> input1(arr1, arr1+n1);
+    vector<int> input2(arr2, arr2+n2);
+    vector<int> input3(arr3, arr3+n3);
+
+    vector<vector<int>> map;
+    map.push_back(intput1);
+    map.push_back(intput2);
+    map.push_back(intput3);
+
     Solution *solution = new Solution();
-//    int n = solution->minPathSum(input1);
+    int n = solution->minPathSum(map);
+    LOGD("n: %d\n", n);
     delete solution;
 }
