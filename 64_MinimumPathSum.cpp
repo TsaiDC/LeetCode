650d770e7c6e23ae6b1d082204e7f6679bdc4c93
TsaiDC <neko.artin@gmail.com>
2020-05-28 01:24:48 +0800

    [#64. Minimum Path Sum]
    Init. 003
    
    git push origin HEAD:master-dev

--- 64_MinimumPathSum.cpp
+++ 64_MinimumPathSum.cpp
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <vector>
 #include <map>
+#include <queue>
 
 #include "apiheader.h"
 
@@ -54,7 +55,57 @@ Explanation: Because the path 1→3→1→1→1 minimizes the sum.
 
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
+    int minPathSum(vector< vector<int> >& grid) {
+        vector<bool> row(grid[0].size(), false);
+        vector< vector<bool> > map (grid.size(), row);
+        vector<int> vrow(grid[0].size(), false);
+        vector< vector<int> > vmap (grid.size(), vrow);
+        queue<int> rQ;
+        queue<int> cQ;
+
+        rQ.push(0);
+        cQ.push(0);
+        map[0][0] = true;
+        
+        int width = grid[0].size();
+        int hight = grid.size();
+        
+        while(!rQ.empty() && !cQ.empty())
+        {
+            int r = rQ.front();
+            int c = cQ.front();
+            rQ.pop();
+            cQ.pop();
+            if(r>=grid.size()) continue;
+            if(c>=grid[0].size()) continue;
+
+            //Left-Top Corner
+            if((r-1)<0 && (c-1)<0) {
+                vmap[r][c] = grid[r][c];
+                map[r][c] = true;                
+            }
+            //Top Row
+            else if((r-1)<0 && (c-1)>=0) {
+                vmap[r][c] = grid[r][c] + vmap[r][c-1];
+                map[r][c] = true;
+            }
+            //Left Column
+            else if((r-1)>=0 && (c-1)<0) {
+                vmap[r][c] = grid[r][c] + vmap[r-1][c];
+                map[r][c] = true;
+            }
+            else {
+                map[r][c] = true;
+                vmap[r][c]= (grid[r][c]+vmap[r-1][c]) > (grid[r][c]+vmap[r][c-1]) ? (grid[r][c]+vmap[r][c-1]) : (grid[r][c]+vmap[r-1][c]);
+            }
+            int rR = r;
+            int rC = c+1;
+            int dR = r+1;
+            int dC = c;
+//TODO
+            rQ.push(r);cQ.push(c+1);
+            rQ.push(r+1);cQ.push(c);
+        }
         return 0;
     }
 };
@@ -73,10 +124,10 @@ void Test_minPathSum()
     vector<int> input2(arr2, arr2+n2);
     vector<int> input3(arr3, arr3+n3);
 
-    vector<vector<int>> map;
-    map.push_back(intput1);
-    map.push_back(intput2);
-    map.push_back(intput3);
+    vector< vector<int> > map;
+    map.push_back(input1);
+    map.push_back(input2);
+    map.push_back(input3);
 
     Solution *solution = new Solution();
     int n = solution->minPathSum(map);
