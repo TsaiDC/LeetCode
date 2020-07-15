6ba3b36f9f976616741f60c433a6a4a05eb69455
TsaiDC <neko.artin@gmail.com>
2020-07-15 17:40:43 +0800

    [#300. Longest Increasing Subsequence]
    Resolved. Good Solution.
    Runtime: 4 ms, faster than 98.43% of C++ online submissions for Longest Increasing Subsequence.
    Memory Usage: 7.8 MB, less than 74.17% of C++ online submissions for Longest Increasing Subsequence.
    
    git push origin HEAD:master-dev

--- 300_LongestIncreasingSub.cpp
+++ 300_LongestIncreasingSub.cpp
@@ -44,6 +44,15 @@ Your algorithm should run in O(n2) complexity.
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
+        return LIS(nums);
+        //return longestIncreasingSubsequence(nums);
+    }
+
+    // Time: O(n^n)
+    // Space: O(nO
+    // Myself solution
+    int LIS(vector<int>& nums)
+    {
         if(nums.empty())
             return 0;
 
@@ -65,13 +74,67 @@ public:
         max = 0;
         for(int i=0; i<stack.size(); ++i)
         {
-//            LOGD("N: %3d, S: %3d\n", nums[i], stack[i]);
             if(stack[i]>=max) {
                 max = stack[i];
             }
         }
 
         return max+1;
+    }
+
+    //
+    // Time: O(nlongn)
+    // Space: O(n)
+    // Ref:
+    // https://segmentfault.com/a/1190000003819886
+    //
+    //Runtime: 4 ms, faster than 98.43% of C++ online submissions for Longest Increasing Subsequence.
+    //Memory Usage: 7.8 MB, less than 74.17% of C++ online submissions for Longest Increasing Subsequence.
+    //
+    int longestIncreasingSubsequence(vector<int>& nums)
+    {
+        if(nums.empty()) return 0;
+
+        int len = 0;
+        vector<int> tails(nums.size(), 0);        
+        tails[0] = nums[0];
+        
+        for(int i=1; i<nums.size(); ++i)
+        {
+            printf("%2d+ ", i); for(int j=0; j<tails.size(); j++){printf("%2d ", tails[j]);} printf("\n");
+            if(nums[i] < tails[0]){
+                tails[0] = nums[i];
+                LOGD("Replace 0: %d\n", nums[i]);
+            }
+            else if(nums[i] > tails[len]){
+                tails[++len] = nums[i];
+                LOGD("Replace %d: %d\n", len, nums[i]);
+            }
+            else {
+                int idx = binarySearch(tails, 0, len, nums[i]);
+                tails[idx] = nums[i];
+                LOGD("Replace %d: %d\n", idx, nums[i]);
+            }
+            printf("%2d- ", i); for(int j=0; j<tails.size(); j++){printf("%2d ", tails[j]);} printf("\n\n");
+        }
+        return len+1;
+    }
+    
+    int binarySearch(vector<int>& tails, int min, int max, int target)
+    {
+        while(min<=max) {
+            int mid = min + (max-min)/2;
+            if(tails[mid] == target) {
+                return mid;
+            }
+            else if(tails[mid]<target) {
+                min = mid+1;
+            }
+            else if(tails[mid]>target) {
+                max = mid-1;
+            }
+        }
+        return min;
     }
 };
 
@@ -81,11 +144,23 @@ void Test_lengthOfLIS()
 
 //    int arr1[] = {10,9,2,5,3,7,101,18};
 //    int arr1[] = {1,3,6,7,9,4,10,5,6};
-    int arr1[] = {2,2};
-    int n = sizeof(arr1)/sizeof(arr1[0]);
+//    int arr1[] = {2,2};
+    int arr1[] = {10,9,2,5,3,4};
+    int n = sizeof(arr1)/sizeof(arr1[0]);       
     vector<int> numbs(arr1, arr1+n);
+    LOGD("Input: ");
+    for(int i=0; i<n; i++)
+    {
+        printf("%2d ",numbs[i]);
+    }
+    printf("\n");
+    
     Solution *solution = new Solution();
     int ans = solution->lengthOfLIS(numbs);
+//    LOGD("Ans: %d\n", ans);
+    ans = solution->LIS(numbs);
     LOGD("Ans: %d\n", ans);
+    ans = solution->longestIncreasingSubsequence(numbs);
+    LOGD("Ans: %d\n", ans);    
     delete solution;
 }
