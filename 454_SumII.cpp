720c977b720fdfaaa483930c6562cc3ed65625b9
TsaiDC <neko.artin@gmail.com>
2020-07-20 22:05:02 +0800

    [#454. 4Sum II]
    Resolved, Time limit exceed. By recursive.
    
    git push origin HEAD:master-dev

--- 454_SumII.cpp
+++ 454_SumII.cpp
@@ -56,16 +56,76 @@ The two tuples are:
 class Solution {
 public:
     int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
-        return 0;
+        map<string, int> mapComb;
+        getComb(A, B, C, D, mapComb);
+        return mapComb.size();
+    }
+    
+    void getComb(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D, map<string, int>& mapComb) {
+        int a, b, c, d;
+        if(A.empty()) return;
+        if(B.empty()) return;
+        if(C.empty()) return;
+        if(D.empty()) return;
+
+        a = A.size()-1;
+        b = B.size()-1;
+        c = C.size()-1;
+        d = D.size()-1;
+        
+        if(A[a]+B[b]+C[c]+D[d] == 0) {
+            char buffer [10] ={'\0'};
+            map<string, int>::iterator iter;
+            sprintf (buffer, "%2d%2d%2d%2d", a, b, c, d);
+            string str = buffer;
+            iter = mapComb.find(str);
+            if(iter == mapComb.end()) {
+                mapComb.insert(pair<string, int>(str, 1));
+                LOGD("(%d, %d, %d, %d) STR: %s\n", a, b, c, d, str.c_str());
+            }            
+        }
+
+        vector<int> AA(A);
+        vector<int> BB(B);
+        vector<int> CC(C);
+        vector<int> DD(D);
+        
+        AA.pop_back();
+        BB.pop_back();
+        CC.pop_back();
+        DD.pop_back();
+        
+        getComb(AA, B, C, D, mapComb);
+        getComb(A, BB, C, D, mapComb);
+        getComb(A, B, CC, D, mapComb);
+        getComb(A, B, C, DD, mapComb);
     }
 };
 
 void Test_fourSumCount()
 {
     LOGD("%s\n", __TIME__);
-    int arr1[] = {10,9,2,5,3,4};
+    // int arr1[] = {1,2};
+    // int arr2[] = {-2,-1};
+    // int arr3[] = {-1,2};
+    // int arr4[] = {0,2};
+    
+    // int arr1[] = {-1,-1};
+    // int arr2[] = {-1,1};
+    // int arr3[] = {-1,1};
+    // int arr4[] = {1,-1};
+    
+    int arr1[] = {1,1,-1,-1};
+    int arr2[] = {-1,-1,1,1};
+    int arr3[] = {1,-1,0,-1};
+    int arr4[] = {1,1,-1,1};
     int n = sizeof(arr1)/sizeof(arr1[0]);       
-    vector<int> numbs(arr1, arr1+n);
+    vector<int> num1(arr1, arr1+n);
+    vector<int> num2(arr2, arr2+n);
+    vector<int> num3(arr3, arr3+n);
+    vector<int> num4(arr4, arr4+n);
     Solution *solution = new Solution();
+    int ans  = solution->fourSumCount(num1,num2,num3,num4);
+    LOGD("Ans: %d\n", ans);
     delete solution;
 }
