4e0701cf81b620689ec2a5dd82f2e9702cf06d57
TsaiDC <neko.artin@gmail.com>
2020-07-24 01:07:27 +0800

    [#378. Kth Smallest Element in a Sorted Matrix]
    Resolved. Ref Solution, Binary Search
    
    git push origin HEAD:master-dev

--- 378_KthSmallestElementInASortedMatrix.cpp
+++ 378_KthSmallestElementInASortedMatrix.cpp
@@ -56,6 +56,32 @@ You may assume k is always valid, 1 ≤ k ≤ n2.
 
 class Solution {
 public:
+#if 1 //Ref Solution
+    int kthSmallest(vector< vector<int> >& matrix, int k) {
+        int n = matrix.size();
+        
+        int left = matrix[0][0];
+        int right = matrix[n-1][n-1];
+        
+        while (left < right) {
+            int midv = (left + right) / 2;
+            
+            int count = 0;  // number of elements no bigger than midv
+            for (int i = 0; i < n; i++) {
+                vector<int>& row = matrix[i];
+                count += std::upper_bound(row.begin(), row.end(), midv) - row.begin();
+            }
+            
+            if (count < k) {
+                left = midv + 1;
+            } else {
+                right = midv;    
+            }
+        }
+        return left;
+    }
+#endif
+#if 0 //My Solution
     int kthSmallest(vector< vector<int> >& matrix, int k) {
         if(matrix.empty())return 0;
         
@@ -80,6 +106,7 @@ public:
         }        
         return (order+min);        
     }
+#endif
 };
 
 void Test_kthSmallest()
