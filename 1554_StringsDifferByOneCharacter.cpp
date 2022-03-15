f1bbf86a7f5e478208fff10fcf78c11036ef8fac
TsaiDC <neko.artin@gmail.com>
2022-03-15 23:53:35 +0800

    [#1554. Strings Differ by One Character]
    Resolved.
    Runtime: 698 ms, faster than 40.14% of C++ online submissions for Strings Differ by One Character.
    Memory Usage: 157.9 MB, less than 44.94% of C++ online submissions for Strings Differ by One Character.
    
    git push origin HEAD:master-dev

--- 1554_StringsDifferByOneCharacter.cpp
+++ 1554_StringsDifferByOneCharacter.cpp
@@ -77,17 +77,87 @@ void Test_GG_differByOne()
 /*
 
 */
+//Time Limit Exceeded
+/*
 class Solution {
 public:
     bool differByOne(vector<string>& dict) {
-        
+        if(dict.empty()) return false;
+        int strLen = dict[0].length();
+        string tmp;
+        char c;
+        unsigned int hasVal, hasVal2;
+        hash<string> hash_obj;
+
+        for(int x=0; x<dict.size(); ++x){
+            for(int i=0; i<strLen; ++i) {
+                tmp = dict[x];
+                c = tmp.at(i);
+                tmp.erase(i, 1);
+                hasVal = hash_obj(tmp);
+                for(int j=x+1; j<dict.size(); ++j){
+                    if(dict[j].at(i) != c) {
+                        tmp = dict[j];
+                        tmp.erase(i, 1);
+                        hasVal2 = hash_obj(tmp);
+                        LOGD("%s (%u) ?= %s (%u)\n", dict[0].c_str(), hasVal, dict[j].c_str(), hasVal2);
+                        if( hasVal2 == hasVal) {                        
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
+*/
+
+/*
+Runtime: 698 ms, faster than 40.14% of C++ online submissions for Strings Differ by One Character.
+Memory Usage: 157.9 MB, less than 44.94% of C++ online submissions for Strings Differ by One Character.
+*/
+class Solution {
+public:
+    bool differByOne(vector<string>& dict) {
+        if(dict.empty()) return false;
+        string tmp;
+        unsigned int hasVal;
+        hash<string> hash_obj;
+        map<unsigned int, string>::iterator iter;
+        map<unsigned int, string> mMap;
+        for(int j=0; j<dict[0].length(); ++j) {
+            for(int i=0; i<dict.size(); ++i) {            
+                tmp = dict[i];
+                tmp.erase(j, 1);
+                hasVal = hash_obj(tmp);
+                iter = mMap.find(hasVal);
+                if(iter != mMap.end()){
+                    return true;
+                }
+                else {
+                    mMap[hasVal] = dict[i];
+                }
+            }
+            mMap.clear();
+        }
+        return false;
     }
 };
 
 void Test_GG_differByOne()
 {
-    LOGD("[CPP] %s\n", __TIME__);   
-    Solution *solution = new Solution();  
+    LOGD("[CPP] %s\n", __TIME__);
+//    string str[] = {"aaaddb","aaaacd","aaacda","aaaaba","aaaccd"};
+//    string str[] = {"abcd","acbd", "aacd"};
+//    string str[] = {"ab","cd","yz"};
+//    string str[] = {"abcde","abaaa","aaade"};
+    string str[] = {"da","ae"};
+    int n = sizeof(str)/sizeof(str[0]);
+    vector<string> input1(str, str+n);
+    Solution *solution = new Solution();
+    bool b = solution->differByOne(input1);
+    LOGD("B: %d\n", b ? 1 : 0);
     delete solution;
 }
 
