45f4f016bca62fd4aa54f0c8047bbf465cf60aa8
Neko <NekoPro@NekodeMacBook-Pro.local>
2022-06-14 00:23:10 +0800

    [#49. Group Anagrams]
    Resolved.
    Runtime: 205 ms, faster than 5.85% of C++ online submissions for Group Anagrams.
    Memory Usage: 25.4 MB, less than 12.28% of C++ online submissions for Group Anagrams.
    
    git push origin HEAD:master-dev

--- 49_GroupAnagrams.cpp
+++ 49_GroupAnagrams.cpp
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <vector>
 #include <map>
+#include <unordered_map>
 
 #include "apiheader.h"
 
@@ -29,6 +30,10 @@ using namespace std;
 //
 // MAP Ref:
 // https://mropengate.blogspot.com/2015/12/cc-map-stl.html
+// 
+// Unorder Map:
+// https://shengyu7697.github.io/std-unordered_map/
+// https://www.sczyh30.com/posts/C-C/cpp-stl-hashmap/
 //
 
 /*
@@ -90,15 +95,18 @@ void Test_AM_groupAnagrams()
 #ifdef _CPPVERSION_
 //C++
 /*
-
+Runtime: 205 ms, faster than 5.85% of C++ online submissions for Group Anagrams.
+Memory Usage: 25.4 MB, less than 12.28% of C++ online submissions for Group Anagrams.
 */
 
 class Solution {
 public:
     vector< vector<string> > groupAnagrams(vector<string>& strs) {
         vector< vector<string> > ans;
+        unordered_map<string, vector<string> > umap;
+        unordered_map<string, vector<string> >::iterator it;
         for(string s: strs) {
-            LOGD("Show: %s\n", s.c_str());
+//            LOGD("Show: %s\n", s.c_str());
             int letterCount[26] = {0};
             for(int i=0; i<s.length(); ++i) {
                 letterCount[s.at(i) - 'a']++;
@@ -106,17 +114,32 @@ public:
             
             string key;
             string dig;
-            LOGD("+++ %s: \n", s.c_str());
+//            LOGD("+++ %s: \n", s.c_str());
             for(int i: letterCount) {
-                 printf("%d", i);
+//                 printf("%d", i);
                  dig = std::to_string(i);
                  key += dig;
                  key += "#";
             }
-            printf("\n");
-            LOGD("--- %s: key: %s\n", s.c_str(), key.c_str());            
+//            printf("\n");
+//            LOGD("--- %s: key: %s\n", s.c_str(), key.c_str());
+            
+            if((it = umap.find(key)) != umap.end()) {
+                it->second.push_back(s);
+//                LOGD("Hit: %s\n", s.c_str());
+            }
+            else {
+                vector<string> row;
+                row.push_back(s);
+                umap.insert(std::make_pair(key, row));
+//                LOGD("Miss: %s\n", s.c_str());
+            }          
         }
 
+        for(it = umap.begin(); it != umap.end(); ++it) {
+//            LOGD("Row Size: %d\n", it->second.size());
+            ans.push_back(it->second);
+        }
         return ans;
     }
 };
@@ -124,13 +147,21 @@ public:
 void Test_AM_groupAnagrams()
 {
     LOGD("[CPP] %s\n", __TIME__);
+    vector< vector<string> > ans;
     string arr1[] = {"eat","tea","tan","ate","nat","bat"};
     int n1 = sizeof(arr1)/sizeof(arr1[0]);
     vector<string> input1(arr1, arr1+n1);
     
     Solution *solution = new Solution();
-    solution->groupAnagrams(input1);
-    delete solution;    
+    ans = solution->groupAnagrams(input1);
+    delete solution;
+    
+    for( vector<string> vs : ans) {
+        for(string str: vs) {
+            printf("%s ", str.c_str());
+        }
+        printf("\n");
+    }    
 }
 
 #endif// _CPPVERSION_
