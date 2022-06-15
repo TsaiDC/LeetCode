1a4e23f461506ef6eebec9430e21602da561bf9e
TsaiDC <neko.artin@gmail.com>
2022-06-15 18:00:33 +0800

    [#79. Word Search]
    Init. 001
    
    git push origin HEAD:master-dev

--- 79_WordSearch.cpp
+++ 79_WordSearch.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <vector>
+#include <queue>
 #include <map>
 
 #include "apiheader.h"
@@ -95,9 +96,60 @@ void Test_AM_exist()
 
 */
 
+typedef struct xpair_t{
+    int i;
+    int j;
+    int ptr;
+    xpair_t(int a, int b, int c):i(a),j(b), ptr(c){}
+}xpair;
+
 class Solution {
 public:
     bool exist(vector< vector<char> >& board, string word) {
+        queue<xpair>TraceQueue;
+        
+        int RecordMap[3][4] = {0};
+        int ptr = 0;
+        
+        for(int i=0; i<board.size(); ++i) {
+            for(int j=0;j<board[i].size(); ++j) {
+                if(board[i][j] == word.at(0)) {
+//                    LOGD("%d, %d\n", i, j);
+                    xpair node(i,j, ptr);
+                    TraceQueue.push(node);
+                    
+                    while(!TraceQueue.empty()) {
+                        node = TraceQueue.front();
+                        TraceQueue.pop();
+                        ptr = node.ptr;
+                        if(ptr == word.length()-1) {
+                            return true;
+                        }
+                        RecordMap[node.i][node.j] = 1;
+                        
+                        
+                        
+                        
+                        for(int ii=node.i-1; ii<node.i+2; ++ii) {
+                            for(int jj = node.j-1; jj<node.j+2; ++jj) {
+                                if(ii<0 || ii>board.size()-1) continue;
+                                if(jj<0 || jj>board[0].size()-1) continue;
+                                if(ii == node.i-1 && jj == node.j-1) continue; //left-up
+                                if(ii == node.i-1 && jj == node.j+1) continue; //left-down
+                                if(ii == node.i+1 && jj == node.j-1) continue; //right-up
+                                if(ii == node.i+1 && jj == node.j+1) continue; //right-down
+                                
+                                if(board[ii][jj] == word.at(ptr+1) && 
+                                   RecordMap[ii][jj] != 1) {
+                                   xpair node1(ii, jj, ptr+1);
+                                   TraceQueue.push(node1);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
         return false;
     }
 };
@@ -107,7 +159,10 @@ void Test_AM_exist()
 {
     LOGD("[CPP] %s\n", __TIME__);
 
-    string word = "ABCCED";
+#if 1
+//    string word = "ABCCED";
+//    string word = "SEE";
+    string word = "ABCB";
     int n;
 
     char arr1[] = {'A','B','C','E'};
@@ -126,7 +181,35 @@ void Test_AM_exist()
     board.push_back(num1);
     board.push_back(num2);
     board.push_back(num3);
-    
+#endif    
+
+#if 0
+//[["a","b"],["c","d"]]
+//"abcd"    
+
+    string word = "abcd";
+    int n;
+
+    char arr1[] = {'a','b'};
+    n = sizeof(arr1)/sizeof(arr1[0]);
+    vector<char> num1(arr1, arr1+n);
+
+    char arr2[] = {'c','d'};
+    n = sizeof(arr2)/sizeof(arr2[0]);
+    vector<char> num2(arr2, arr2+n);
+
+    vector< vector<char> >board;
+    board.push_back(num1);
+    board.push_back(num2);
+#endif
+
+#if 0
+//["A","B","C","E"],
+//["S","F","E","S"],
+//["A","D","E","E"]
+//"ABCESEEEFS"
+
+#endif    
     Solution *solution = new Solution();    
     bool b = solution->exist(board, word);    
     delete solution;
