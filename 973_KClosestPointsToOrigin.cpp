d3f6256b493f11048678ed777debf55fca3dad69
Neko <NekoPro@NekodeMacBook-Pro.local>
2022-07-01 01:36:21 +0800

    [#973. K Closest Points to Origin]
    Resolved. Self-Solution
    Runtime: 1616 ms, faster than 5.00% of C++ online submissions for K Closest Points to Origin.
    Memory Usage: 60.4 MB, less than 82.20% of C++ online submissions for K Closest Points to Origin.
    
    git push origin HEAD:master-dev

--- 973_KClosestPointsToOrigin.cpp
+++ 973_KClosestPointsToOrigin.cpp
@@ -97,32 +97,67 @@ void Test_AM_kClosest()
 #ifdef _CPPVERSION_
 //C++
 /*
-
+Self-Solution
+Runtime: 1616 ms, faster than 5.00% of C++ online submissions for K Closest Points to Origin.
+Memory Usage: 60.4 MB, less than 82.20% of C++ online submissions for K Closest Points to Origin.
 */
 class Solution {
 public:
     vector< vector<int> > kClosest(vector< vector<int> >& points, int k) {
         vector< vector<int> >ans;
         int left = 0, right = points.size()-1, idx;
-        idx = getSortK(points, left, right-1, right);
-        LOGD("idx: %d\n", idx);
+        while(left < right) {
+            idx = getSortK(points, left, right);
+//            LOGD("Search %d->%d => Idx: %d\n", left, right, idx);
+
+            if(idx==k-1) {
+//                LOGD("Find!\n");
+                break;
+            }
+            else if(idx < k-1) {
+                left = idx+1;               
+            }
+            else {
+                right = idx-1;
+            }
+        }
+
+        for(int i=0; i<k; ++i) {
+            ans.push_back(points[i]);
+        }
+        
         return ans;
     }
     
-    int getSortK(vector< vector<int> >& point, int i, int j, int k) {                
+    int getSortK(vector< vector<int> >& point, int i, int j) {
+        int k = j;
+        int x, y;
         long long lengthK = getLength(point[k][0], point[k][1]);
         
         while(i<j) {
             while(getLength(point[i][0], point[i][1]) < lengthK && i<j) ++i;
-            while(getLength(point[j][0], point[j][1]) > lengthK && i<j) --j;
-            //Swap(i, k)
-            int x = point[i][0];
-            int y = point[i][1];
-            point[i][0] = point[k][0];
-            point[i][1] = point[k][1];
-            point[i][0] = x;
-            point[i][1] = y;
+            while(getLength(point[j][0], point[j][1]) >= lengthK && i<j) --j;
+//            LOGD("Swap(%d, %d)\n", i, j);
+            x = point[i][0];
+            y = point[i][1];
+            point[i][0] = point[j][0];
+            point[i][1] = point[j][1];
+            point[j][0] = x;
+            point[j][1] = y;         
         }
+//        LOGD("Swap(%d, %d)\n", i, k);
+        x = point[i][0];
+        y = point[i][1];
+//        LOGD("%d %d, %d %d\n", point[i][0], point[i][1], point[k][0], point[k][1]);
+        point[i][0] = point[k][0];
+        point[i][1] = point[k][1];
+        point[k][0] = x;
+        point[k][1] = y;
+//        LOGD("%d %d, %d %d\n", point[i][0], point[i][1], point[k][0], point[k][1]);
+//        for(auto &a:point) {
+//            LOGD("Point: %d, %d\n", a[0], a[1]);
+//        }
+        
         return i;
     }
     
@@ -131,11 +166,43 @@ public:
     }
 };
 
+
+#if 0
+/*
+Ref Solution
+Runtime: 314 ms, faster than 66.25% of C++ online submissions for K Closest Points to Origin.
+Memory Usage: 57 MB, less than 97.23% of C++ online submissions for K Closest Points to Origin.
+*/
+
+//Ref Solution, Sort with a compairiter
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        // Sort the vector with a custom lambda comparator function
+        sort(points.begin(), points.end(), [&](vector<int>& a, vector<int>& b) {
+            return squaredDistance(a) < squaredDistance(b);
+        });
+        
+        // Return the first k elements of the sorted vector
+        return vector<vector<int>>(points.begin(), points.begin() + k);
+    }
+    
+private:
+    int squaredDistance(vector<int>& point) {
+        // Calculate and return the squared Euclidean distance
+        return point[0] * point[0] + point[1] * point[1];
+    }
+};
+
+#endif
+
 void Test_AM_kClosest()
 {
     LOGD("[CPP] %s\n", __TIME__);
     vector< vector<int> > ans;
-//    [[3,3],[5,-1],[-2,4]], k = 2
+#if 1
+//  [[3,3],[5,-1],[-2,4]], k = 2
+    int k = 2;
     int arr1[] = {3,3};
     int n1 = sizeof(arr1)/sizeof(arr1[0]);
     vector<int> input1(arr1, arr1+n1);
@@ -150,10 +217,28 @@ void Test_AM_kClosest()
     input.push_back(input1);
     input.push_back(input2);
     input.push_back(input3);
-    
+#endif
+#if 0
+//  [[1,3],[-2,2]], k = 1 
+    int k = 1;
+    int arr1[] = {1,3};
+    int n1 = sizeof(arr1)/sizeof(arr1[0]);
+    vector<int> input1(arr1, arr1+n1);
+    int arr2[] = {-2,2};
+    int n2 = sizeof(arr2)/sizeof(arr2[0]);
+    vector<int> input2(arr2, arr2+n2);
+
+    vector< vector<int> > input;
+    input.push_back(input1);
+    input.push_back(input2);
+#endif
     Solution *solution = new Solution();
-    ans = solution->kClosest(input, 2);
+    ans = solution->kClosest(input, k);
     delete solution;
+    
+    for(int i=0; i<ans.size(); ++i) {
+        LOGD("Ans: [%d, %d]\n", ans[i][0], ans[i][1]);
+    }
 }
 
 #endif// _CPPVERSION_
