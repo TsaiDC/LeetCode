97d0e49c2b9e37e0e714f0fe225b6609b35e387f
TsaiDC <neko.artin@gmail.com>
2022-07-02 21:37:57 +0800

    [#863. All Nodes Distance K in Binary Tree]
    Resolved. Self Solution, Tree->Graph->BFS
    Runtime: 12 ms, faster than 39.56% of C++ online submissions for All Nodes Distance K in Binary Tree.
    Memory Usage: 15.6 MB, less than 11.14% of C++ online submissions for All Nodes Distance K in Binary Tree.
    
    git push origin HEAD:master-dev

--- 863_AllNodesDistanceKinBinaryTree.cpp
+++ 863_AllNodesDistanceKinBinaryTree.cpp
@@ -100,7 +100,9 @@ void Test_AM_distanceK()
 #ifdef _CPPVERSION_
 //C++
 /*
-
+Self Solution, Tree->Graph->BFS
+Runtime: 12 ms, faster than 39.56% of C++ online submissions for All Nodes Distance K in Binary Tree.
+Memory Usage: 15.6 MB, less than 11.14% of C++ online submissions for All Nodes Distance K in Binary Tree.
 */
 
 //Definition for a binary tree node.
@@ -113,16 +115,141 @@ struct TreeNode {
 
 class Solution {
 public:
+    TreeNode* pTarget = NULL;
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        vector<int> ans;    
+        unordered_map<TreeNode*, TreeNode*>treeMap;    //<Node, Parent of Node
+        unordered_map<int, int>recordMap;
+        queue<TreeNode*> treeQueue;
+
+        //Tree to Map, O(n)
+        initMap(treeMap, root, target);
+
+        //Map to graph, O(n)
+        initGraph(treeMap);
+/*
+        for(auto &p:treeMap) {
+            if(p.second) {
+                LOGD("Grap: node: %d, parent: %d\n", p.first->val, p.second->val);
+            }
+            else {
+                LOGD("Grap: node: %d, parent: NULL\n", p.first->val);
+            }
+        }
+*/
+        //Find the answer
+        if(!pTarget) return ans;
+        treeQueue.push(pTarget);        
+        for(int i=0; i<k; ++i) {
+            getAns(treeQueue, treeMap, recordMap);
+        }
         
+        while(!treeQueue.empty()) {
+            TreeNode* pTop = treeQueue.front();
+            treeQueue.pop();
+            if(pTop) {
+                ans.push_back(pTop->val);
+            }
+        }            
+
+        return ans;
+    }
+    
+    int getAns(queue<TreeNode*>& Q, unordered_map<TreeNode*, TreeNode*>& treeMap, unordered_map<int, int>& recordMap) {
+        int count = 0;
+        unordered_map<TreeNode*, TreeNode*>::iterator iter;
+        queue<TreeNode*>newQ;
+//        LOGD("+++++\n");
+        while(!Q.empty()) {
+            TreeNode* pTop = Q.front();
+            Q.pop();
+            if(!pTop)continue;
+//            LOGD("Get: %d => ", pTop->val);
+            recordMap[pTop->val] = 1;
+
+            if(pTop->left && 
+               recordMap.find(pTop->left->val) == recordMap.end()) {
+               newQ.push(pTop->left);  //left child
+               ++count;                    
+//               printf("%d ", pTop->left->val);
+            }
+            if(pTop->right && 
+               recordMap.find(pTop->right->val) == recordMap.end()) {
+               newQ.push(pTop->right);  //left child
+               ++count;                    
+//               printf("%d ", pTop->left->val);
+            }
+            if((iter = treeMap.find(pTop))!= treeMap.end()) {
+                if(iter->second != NULL &&
+                   recordMap.find(iter->second->val) == recordMap.end()) {
+                    newQ.push(iter->second);  //parent
+                    ++count;
+//                    printf("%d ", iter->second->val);
+                }
+            }
+        }
+
+        Q = newQ;
+//        LOGD("-----\n");
+        return count;
+    }
+    void initMap(unordered_map<TreeNode*, TreeNode*>& treeMap, TreeNode* root, TreeNode* target)
+    {
+        if(!root) return;
+        if(root->val == target->val) pTarget = target;
+        treeMap[root] = NULL;        
+        initMap(treeMap, root->left, target);
+        initMap(treeMap, root->right, target);
+    }
+    
+    void initGraph(unordered_map<TreeNode*, TreeNode*>& treeMap)
+    {
+        unordered_map<TreeNode*, TreeNode*>::iterator iter;
+        unordered_map<TreeNode*, TreeNode*>::iterator iChild;
+        for(iter=treeMap.begin(); iter!=treeMap.end(); ++iter) {
+            TreeNode* pLeft = iter->first->left;
+            TreeNode* pRight = iter->first->right;
+            if(pLeft) {
+                iChild = treeMap.find(pLeft);
+                treeMap[iChild->first] = iter->first;
+            }
+            if(pRight) {
+                iChild = treeMap.find(pRight);
+                treeMap[iChild->first] = iter->first;
+            }
+        }
     }
 };
 
 void Test_AM_distanceK()
 {
     LOGD("[CPP] %s\n", __TIME__);
+    TreeNode* node[9];
+    TreeNode* pRoot;
+    for(int i=0; i<9; ++i) {
+        node[i] = new TreeNode(i);
+    }
+    pRoot = node[3];
+    node[3]->left = node[5];
+    node[3]->right = node[1];
+    node[5]->left = node[6];
+    node[5]->right = node[2];
+    node[1]->left = node[0];
+    node[1]->right = node[8];
+    node[2]->left = node[7];
+    node[2]->right = node[4];
+    vector<int>ans;
     Solution *solution = new Solution();
-    delete solution;    
+    ans = solution->distanceK(pRoot, node[5], 2);
+    delete solution;
+    
+    for(int a: ans) {
+        LOGD("Ans: %d\n", a);
+    }
+    
+    for(int i=0; i<9; ++i) {
+        delete node[i];
+    }
 }
 
 #endif// _CPPVERSION_
