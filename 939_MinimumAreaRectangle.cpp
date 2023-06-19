d5889f24d56cca6dcba3f3bf08389a433dc8984e
Neko <NekoPro@NekodeMacBook-Pro.local>
2023-06-20 01:35:22 +0800

    [#939. Minimum Area Rectangle]
    Init. 1
    
    git push origin HEAD:master-dev

--- 939_MinimumAreaRectangle.cpp
+++ 939_MinimumAreaRectangle.cpp
@@ -8,6 +8,7 @@
 #include <queue>
 #include <map>
 #include <stack>
+#include <unordered_set>
 #include <unordered_map>
 #include <algorithm>
 
@@ -88,10 +89,48 @@ void Test_GG_minAreaRect()
 
 */
 
+struct pair_hash
+{
+    template <class T1, class T2>
+    std::size_t operator () (std::pair<T1, T2> const &pair) const
+    {
+        std::size_t h1 = std::hash<T1>()(pair.first);
+        std::size_t h2 = std::hash<T2>()(pair.second);
+ 
+        return h1 ^ h2;
+    }
+};
+
 class Solution {
 public:
     int minAreaRect(vector<vector<int>>& points) {
+        unordered_set< pair<int,int>, pair_hash >pointsMap;
+
+        int area = 0;
+
+        for(auto a: points) {
+            LOGD("Put (%d, %d)\n", a[0], a[1]);
+            pointsMap.insert(std::make_pair(a[0], a[1]));
+        }
         
+
+        for(int i = 0; i<points.size(); ++i) {
+            for(int j=1+1; j<points.size(); ++j) {
+                pair<int, int> node1 = make_pair(points[i][0], points[j][1]);
+                pair<int, int> node2 = make_pair(points[j][0], points[i][1]);
+                
+                LOGD("(%d, %d) -> (%d, %d) \n", points[i][0], points[i][1], points[j][0], points[j][1]);
+                if(pointsMap.count(node1) > 0 && pointsMap.count(node2) > 0) {
+
+                    int tmpArea = std::abs(points[i][0] - points[j][0]) * 
+                                  std::abs(points[i][1] - points[j][1]);
+                    area = area < tmpArea ? tmpArea : area; 
+                    LOGD("Hit Area: %d\n", tmpArea);
+                }
+            }
+        }
+
+        return area;
     }
 };
 
@@ -102,8 +141,26 @@ void Test_GG_minAreaRect()
 //    int n1 = sizeof(arr1)/sizeof(arr1[0]);
 //    vector<string> input1(arr1, arr1+n1);
 
+//   [[1,1],[1,3],[3,1],[3,3],[4,1],[4,3]]
+    vector<int> v1{1,1};
+    vector<int> v2{1,3};
+    vector<int> v3{3,1};
+    vector<int> v4{3,3};
+    vector<int> v5{4,1};
+    vector<int> v6{3,3};
+    
+    vector<vector<int>>input;
+    input.push_back(v1);
+    input.push_back(v2);
+    input.push_back(v3);
+    input.push_back(v4);
+    input.push_back(v5);
+    input.push_back(v6);
+    
     LOGD("[CPP] %s\n", __TIME__);
     Solution *solution = new Solution();
+    int ans = solution->minAreaRect(input);
+    LOGD("Ans: %d\n", ans)
     delete solution;
 }
 
