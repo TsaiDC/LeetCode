11c755c3cf82bab598510f0fe00ce15fce62bd6f
TsaiDC <neko.artin@gmail.com>
2023-07-05 01:21:28 +0800

    [329. Longest Increasing Path in a Matrix]
    Resolved.
    Runtime: 111 ms Beats 18.52%
    Memory: 22.2 MB Beats 19.28%
    
    git push origin HEAD:master-dev

--- 329_LongestIncreasingPath.cpp
+++ 329_LongestIncreasingPath.cpp
@@ -99,25 +99,99 @@ void Test_GG_longestIncreasingPath()
 #ifdef _CPPVERSION_
 //C++
 /*
-
+Self, 20230704
+Runtime: 111 ms Beats 18.52%
+Memory: 22.2 MB Beats 19.28%
 */
 
+typedef struct node {
+    int val;
+    int x;
+    int y;    
+}node_t;
+
+bool compare(node_t a, node_t b) {
+    return a.val < b.val;
+}
 class Solution {
 public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int ret = 0;
+        vector<node_t> nodeVec;
+        vector<vector<int>> matrixCount;
         
+        for(int i=0; i<matrix.size(); ++i) {
+            for(int j=0; j<matrix[0].size(); ++j) {
+                node_t n;
+                n.val = matrix[i][j];
+                n.x =j;
+                n.y =i;
+                nodeVec.push_back(n);
+            }
+            
+            //Init count map
+            vector<int> v(matrix[i].size(), 0);
+            matrixCount.push_back(v);
+        }
+        
+        std::sort(nodeVec.begin(), nodeVec.end(), compare);
+        
+        for(auto n : nodeVec) {
+//            LOGD("Sorted V: %d\n", n.val);
+            int count = getTheSmallest(n, matrix, matrixCount);
+            if(count > ret) ret = count;
+        }
+        
+        return ret;
+    }
+    
+    int getTheSmallest(node_t& n, vector<vector<int>>& matrix, vector<vector<int>>& matrixCount) {
+        int offset_count = 4;
+        int offset[][2] = {         {0, -1},
+                           {-1, 0},          {1,  0},
+                                    {0,  1},
+                           };
+
+        int x, y, maxCount= 0;
+        for(int i=0; i<offset_count; ++i) {
+            x = offset[i][0] + n.x;
+            y = offset[i][1] + n.y;
+//            LOGD("Node: (%d, %d) -> (%d, %d)\n", n.y, n.x, y, x);
+
+            if(x<0 || x>=matrix[0].size()) continue;
+            if(y<0 || y>=matrix.size()) continue;
+            if(matrix[y][x] >= n.val) continue;
+            if (matrixCount[y][x] > maxCount) {
+                maxCount = matrixCount[y][x];
+            }
+
+        }
+        matrixCount[n.y][n.x] = maxCount + 1;        
+/*
+        LOGD("(%d, %d)\n", n.y, n.x);
+        for(auto r : matrixCount) {
+            for(auto a : r) {
+                printf("%d ", a);
+            }
+            printf("\n");
+        }
+*/
+        return maxCount + 1;
     }
 };
 
 void Test_GG_longestIncreasingPath()
 {
-//    vector< vector<string> > ans;
-//    string arr1[] = {"eat","tea","tan","ate","nat","bat"};
-//    int n1 = sizeof(arr1)/sizeof(arr1[0]);
-//    vector<string> input1(arr1, arr1+n1);
+    //[[9,9,4],[6,6,8],[2,1,1]]
+    vector<int> L1{9,9,4};
+    vector<int> L2{6,6,8};
+    vector<int> L3{2,1,1};
+    vector<vector<int>> input{L1, L2, L3};
 
     LOGD("[CPP] %s\n", __TIME__);
     Solution *solution = new Solution();
+    int ret = solution->longestIncreasingPath(input);
+    LOGD("Ret: %d\n", ret);
     delete solution;
 }
 
