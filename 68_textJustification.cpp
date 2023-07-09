ec10475e821e1e6ac50291155cf8159cf8700745
TsaiDC <neko.artin@gmail.com>
2023-07-09 18:39:08 +0800

    [#68. Text Justification]
    Resolved.
    Ref Solution
    Runtime: 0 ms Beats 100%
    Memory: 7.3 MB Beats 64.58%
    
    git push origin HEAD:master-dev

--- 68_textJustification.cpp
+++ 68_textJustification.cpp
@@ -127,26 +127,91 @@ void Test_GG_fullJustify()
 #ifdef _CPPVERSION_
 //C++
 /*
-
+Ref Solution
+Runtime: 0 ms Beats 100%
+Memory: 7.3 MB Beats 64.58%
 */
 
 class Solution {
 public:
     vector<string> fullJustify(vector<string>& words, int maxWidth) {
-        
+        int left = 0; 
+        vector<string> result;
+
+        while (left < words.size()) {
+            int right = findRight(left, words, maxWidth);       
+            string str = justify(left, right, words, maxWidth);
+            result.push_back(str);
+            left = right + 1;
+        }
+
+        return result;
+    }
+
+private:
+    int findRight(int left, vector<string>& words, int maxWidth) {
+        int right = left;
+        int sum = words[right++].length();
+
+        while (right < words.size() && (sum + 1 + words[right].length()) <= maxWidth)
+            sum += 1 + words[right++].length();
+
+        return right - 1;
+    }
+
+    string justify(int left, int right, vector<string>& words, int maxWidth) {        
+        if (right - left == 0) return padResult(words[left], maxWidth);
+
+        bool isLastLine = (right == words.size() - 1);
+        int numSpaces = right - left;
+        int totalSpace = maxWidth - wordsLength(left, right, words);
+
+        string space = isLastLine ? " " : blank(totalSpace / numSpaces);
+        int remainder = isLastLine ? 0 : totalSpace % numSpaces;
+
+        string result;
+        for (int i = left; i <= right; i++) {
+            result += words[i];
+            result += space;
+            if(remainder-- > 0) {
+                result += " ";
+            }
+        }
+        result.erase(result.find_last_not_of(" \n\r\t")+1);        
+        return padResult(result, maxWidth);
+    }
+
+    int wordsLength(int left, int right, vector<string>& words) {
+        int wordsLength = 0;
+        for (int i = left; i <= right; i++) wordsLength += words[i].length();
+        return wordsLength;
+    }
+
+    string padResult(string result, int maxWidth) {
+        return result + blank(maxWidth - result.length());
+    }
+
+    string blank(int length) {
+        if(length<=0) return "";
+        string s(length, ' ');
+        return s;
     }
 };
 
 void Test_GG_fullJustify()
-{
-//    vector< vector<string> > ans;
-//    string arr1[] = {"eat","tea","tan","ate","nat","bat"};
-//    int n1 = sizeof(arr1)/sizeof(arr1[0]);
-//    vector<string> input1(arr1, arr1+n1);
+{ 
+    vector<string> input{"This", "is", "an", "example", "of", "text", "justification."};
+    int maxWidth = 16;
 
+    vector<string> ans;
     LOGD("[CPP] %s\n", __TIME__);
     Solution *solution = new Solution();
+    ans = solution->fullJustify(input, maxWidth);
     delete solution;
+    
+    for(auto s : ans) {
+        LOGD("[ANS] %s\n",s.c_str());
+    }
 }
 
 #endif// _CPPVERSION_
